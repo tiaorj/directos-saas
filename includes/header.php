f0098c303468ca1f22ac1fe19a0350d4900c93bb
tiaorj <tiaorj@gmail.com>
2026-05-29 11:30:10 -0300

    Corrige caminho do CSS usando APP_URL

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
+<?php
+require_once __DIR__ . "/../config/config.php";
+
+$baseUrl = rtrim(APP_URL, "/");
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,7 +16,7 @@
     >
 
     <link 
-        href="/sistema-os-php-sqlserver/assets/css/directos.css" 
+        href="<?= htmlspecialchars($baseUrl) ?>/assets/css/directos.css" 
         rel="stylesheet"
     >
 </head>
